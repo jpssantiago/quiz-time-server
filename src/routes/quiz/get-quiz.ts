72bb2d103ce4656531c3274ad724866216c76d77
João Pedro Santiago <joao@joaosantiago.com.br>
2024-06-01 14:30:22 -0300

    feat: changed the identification from _id to pin

--- src/routes/quiz/get-quiz.ts
+++ src/routes/quiz/get-quiz.ts
@@ -1,10 +1,10 @@
 import { FastifyRequest, FastifyReply } from "fastify"
 import { z } from "zod"
 
-import { getQuizById } from "../../services/quiz-service"
+import { getQuizByPin } from "../../services/quiz-service"
 
 const schema = z.object({
-    id: z.string()
+    pin: z.string()
 })
 
 export async function getQuiz(request: FastifyRequest, response: FastifyReply) {
@@ -13,7 +13,7 @@ export async function getQuiz(request: FastifyRequest, response: FastifyReply) {
         return response.code(400).send({ err: "bad request" })
     }
 
-    const quiz = await getQuizById(parsed.id)
+    const quiz = await getQuizByPin(parsed.pin)
     if (!quiz) {
         return response.code(404).send({ err: "quiz does not exist" })
     }
--- src/routes/quiz/quiz.ts
+++ src/routes/quiz/quiz.ts
@@ -3,5 +3,5 @@ import { FastifyInstance } from "fastify"
 import { getQuiz } from "./get-quiz"
 
 export default async function(app: FastifyInstance) {
-    app.get("/quiz/:id", getQuiz)
+    app.get("/quiz/:pin", getQuiz)
 }
