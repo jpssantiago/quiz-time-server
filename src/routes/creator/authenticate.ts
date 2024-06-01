1ed670ec77783002502be8891a5fef1d7f9b2870
João Pedro Santiago <joao@joaosantiago.com.br>
2024-06-01 15:28:26 -0300

    hotfix: comparing password with hash

--- src/routes/creator/authenticate.ts
+++ src/routes/creator/authenticate.ts
@@ -1,6 +1,8 @@
 import { FastifyRequest, FastifyReply } from "fastify"
 import { z } from "zod"
 
+import { getQuizByPin } from "../../services/quiz-service"
+import { comparePasswordWithHash } from "../../services/bcrypt-service"
 import { generateToken } from "../../services/token-service"
 
 const schema = z.object({
@@ -14,10 +16,17 @@ export async function authenticate(request: FastifyRequest, response: FastifyRep
         return response.code(400).send({ err: "bad request" })
     }
 
-    if (parsed.password != "password123") { // temporary password
+    const quiz = await getQuizByPin(parsed.pin)
+    if (!quiz) {
+        return response.code(404).send({ err: "quiz does not exist" })
+    }
+
+    const matched = await comparePasswordWithHash(parsed.password, quiz.password)
+    if (!matched) {
         return response.code(401).send({ err: "invalid password" })
     }
 
     const token = generateToken(parsed.pin)
-    response.send({ token })
+    delete (quiz as {password?: string}).password
+    response.send({ token, quiz })
 }
